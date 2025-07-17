cfdcc2fb86006bf0e3df8c4dc259de57b9809e16
Shoaibawan005 <shoaibabbas479@gmail.com>
2025-07-17 12:32:34 +0500

    add the Scoped pattern in program.cs file

--- Store.API/Program.cs
+++ Store.API/Program.cs
@@ -1,5 +1,9 @@
 using Store.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Store.Infrastructure.IRepositries;
+using Store.Infrastructure.Repositries;
+using Store.Application.IServices;
+using Store.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,14 +11,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUserRepo, UserRepo>();
+builder.Services.AddScoped<IProductRepo, ProductRepo>();
+builder.Services.AddScoped<IOrderRepo, OrderRepo>();
+builder.Services.AddScoped<IUserSRV, UserSRV>();
+builder.Services.AddScoped<IProductSRV, ProductSRV>();
+builder.Services.AddScoped<IOrderSRV, OrderSRV>();
+
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection"),
         b => b.MigrationsAssembly("Store.Infrastructure")
 ));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
